9af1d448b600bb93642a915517e7466f03570ade
Molema.Lema <lemamolema@hotmail.fr>
2019-12-28 01:38:38 +0000

    Added another ressource

--- main.tf
+++ main.tf
@@ -16,3 +16,17 @@ resource "aws_instance" "fa-bookingteam-dev-01" {
     volume_size          	= "15"
   }
 }
+
+resource "aws_instance" "fa-bookingteam-dev-02" {
+  ami           			= "ami-08947d0de9ebe8e98"
+  instance_type 			= "t2.micro"
+  vpc_security_group_ids 	= ["sg-0008be08e1a39844f"]
+  key_name   				= "lemaxdevkey"
+  
+  tags = {
+    Name 					= "fa-bookingteam-dev-02"
+  }
+  root_block_device {
+    volume_size          	= "15"
+  }
+}
