a3dbd85920a5d242bd2231c6d686d01e01e835f3
Molema.Lema <lemamolema@hotmail.fr>
2019-12-28 21:53:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,32 @@ provider "aws" {
   region     = "eu-west-1"
 }
 
+resource "aws_instance" "production" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "bookingteam-dev-01"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+}
+
+resource "aws_instance" "uat" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "bookingteam-dev-01"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+}
+
 resource "aws_instance" "bookingteam-dev-01" {
   ami  = "ami-0ee2cc04e6a530d94"
   instance_type  = "t2.micro"
@@ -22,7 +48,57 @@ resource "aws_instance" "bookingteam-dev-02" {
   vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
   key_name  = "lemaxdevkey"  
   tags = {
-    Name  = "bookingteam-dev--02"
+    Name  = "bookingteam-dev-02"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+  
+resource "aws_instance" "paymentteam-dev-01" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "paymentteam-dev-01"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+}
+
+resource "aws_instance" "paymentteam-dev-02" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "paymentteam-dev--02"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+  
+resource "aws_instance" "skywardsteam-dev-01" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "skywardsteam-dev-01"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+}
+
+resource "aws_instance" "skywardsteam-dev-02" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "skywardsteam-dev--02"
   }
   root_block_device {
     volume_size  = "15"
