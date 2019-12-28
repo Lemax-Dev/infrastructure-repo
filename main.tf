56b20859e91736c64e4dd775541d1f948e711a0c
Molema.Lema <lemamolema@hotmail.fr>
2019-12-28 21:18:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,20 @@ resource "aws_instance" "bookingteam-dev-01" {
   vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
   key_name  = "lemaxdevkey"  
   tags = {
-    Name  = "bookingteam-dev---01"
+    Name  = "bookingteam-dev-01"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+}
+
+resource "aws_instance" "bookingteam-dev-02" {
+  ami  = "ami-0ee2cc04e6a530d94"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "bookingteam-dev-02"
   }
   root_block_device {
     volume_size  = "15"
