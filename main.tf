3a2ba25437e5bc8ae2a16b2f09478c00e40f681e
Molema.Lema <lemamolema@hotmail.fr>
2019-12-29 07:47:59 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ resource "aws_subnet" "production_subnet" {
 resource "aws_subnet" "uat_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.96.0/20"
+  assign_ipv6_address_on_creation = "true"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "uat_subnet"
@@ -23,6 +25,8 @@ resource "aws_subnet" "uat_subnet" {
 resource "aws_subnet" "bookingteam_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.112.0/20"
+  assign_ipv6_address_on_creation = "true"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "bookingteam_subnet"
@@ -31,8 +35,21 @@ resource "aws_subnet" "bookingteam_subnet" {
 resource "aws_subnet" "skywards_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.128.0/20"
+  assign_ipv6_address_on_creation = "true"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "skywards_subnet"
   }
 }
+resource "aws_subnet" "tooling_subnet" {
+  vpc_id     = "vpc-9c7fedf8"
+  cidr_block = "172.31.128.0/20"
+  assign_ipv6_address_on_creation = "true"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "skywards_subnet"
+  }
+}
+
