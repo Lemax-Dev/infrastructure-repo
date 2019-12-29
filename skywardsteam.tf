4ee63e6017fb694c2ff2747abc3e96a4bda3a04e
Molema.Lema <lemamolema@hotmail.fr>
2019-12-29 07:56:03 +0000

    Update skywardsteam.tf

--- skywardsteam.tf
+++ skywardsteam.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "skywardsteam-dev-01" {
-  ami  = "ami-0ee2cc04e6a530d94"
   instance_type  = "t2.micro"
   vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
   key_name  = "lemaxdevkey"  
+  ami  = "ami-0fd9f6305e5904533"
+  subnet_id = "subnet-0ea2e7fc082fe7a0e"
   tags = {
     Name  = "skywardsteam-dev-01"
   }
