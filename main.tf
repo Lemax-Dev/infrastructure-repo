d545f0ff8e8cbbd81042cc599757ba1cb0417256
Molema.Lema <lemamolema@hotmail.fr>
2019-12-29 08:12:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_subnet" "production_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.80.0/20"
-  assign_ipv6_address_on_creation = "true"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -15,7 +14,6 @@ resource "aws_subnet" "production_subnet" {
 resource "aws_subnet" "uat_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.96.0/20"
-  assign_ipv6_address_on_creation = "true"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -25,7 +23,6 @@ resource "aws_subnet" "uat_subnet" {
 resource "aws_subnet" "bookingteam_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.112.0/20"
-  assign_ipv6_address_on_creation = "true"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -35,7 +32,6 @@ resource "aws_subnet" "bookingteam_subnet" {
 resource "aws_subnet" "skywards_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.128.0/20"
-  assign_ipv6_address_on_creation = "true"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -45,7 +41,6 @@ resource "aws_subnet" "skywards_subnet" {
 resource "aws_subnet" "tooling_subnet" {
   vpc_id     = "vpc-9c7fedf8"
   cidr_block = "172.31.128.0/20"
-  assign_ipv6_address_on_creation = "true"
   map_public_ip_on_launch = "true"
 
   tags = {
