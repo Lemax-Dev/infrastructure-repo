f4d4366fb0f50c9e81bb5375b3dc590d3f428d81
Molema.Lema <lemamolema@hotmail.fr>
2019-12-29 06:55:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,35 @@ provider "aws" {
   profile    = "default"
   region     = "eu-west-1"
 }
+resource "aws_subnet" "production_subnet" {
+  vpc_id     = "vpc-9c7fedf8"
+  cidr_block = "172.31.1.0/20"
+
+  tags = {
+    Name = "Main"
+  }
+}
+resource "aws_subnet" "uat_subnet" {
+  vpc_id     = "vpc-9c7fedf8"
+  cidr_block = "172.31.2.0/20"
+
+  tags = {
+    Name = "Main"
+  }
+}
+resource "aws_subnet" "bookingteam_subnet" {
+  vpc_id     = "vpc-9c7fedf8"
+  cidr_block = "172.31.3.0/20"
+
+  tags = {
+    Name = "Main"
+  }
+}
+resource "aws_subnet" "skywards_subnet" {
+  vpc_id     = "vpc-9c7fedf8"
+  cidr_block = "172.31.4.0/20"
+
+  tags = {
+    Name = "Main"
+  }
+}
