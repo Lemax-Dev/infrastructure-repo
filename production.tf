c4febecd5751e68fd62c111c516af210bd11ddfd
Molema.Lema <lemamolema@hotmail.fr>
2019-12-29 07:11:55 +0000

    Update production.tf

--- production.tf
+++ production.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "production" {
-  ami  = "ami-0fd9f6305e5904533"
   instance_type  = "t2.micro"
   vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  ami  = "ami-0fd9f6305e5904533"
   subnet_id = "subnet-010f2b1e0001d1701"
   key_name  = "lemaxdevkey"  
   tags = {
