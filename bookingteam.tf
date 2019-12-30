358e5180f2b3dd50bf242c7550cdeec2ccc0f032
Lemax-Dev <lemamolema@hotmail.fr>
2019-12-30 09:22:43 -0500

    Jenkins CI/CD automated build run

--- bookingteam.tf
+++ bookingteam.tf
@@ -11,3 +11,15 @@ resource "aws_instance" "bookingteam-dev-01" {
     volume_size  = "15"
   }
 }
+resource "aws_instance" "bookingteam-dev-4558" {
+  ami  = "ami-0fd9f6305e5904533"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
+  key_name  = "lemaxdevkey"  
+  tags = {
+    Name  = "bookingteam-dev-4558"
+  }
+  root_block_device {
+    volume_size  = "15"
+  }
+}
