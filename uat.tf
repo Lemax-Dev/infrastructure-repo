846b8d81270ad669d57719247c58a8f91da2d5d5
Molema.Lema <lemamolema@hotmail.fr>
2020-01-19 10:17:51 +0000

    Update uat.tf

--- uat.tf
+++ uat.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "uat" {
   instance_type  = "t2.micro"
   vpc_security_group_ids  = ["sg-0008be08e1a39844f"]
   key_name  = "lemaxdevkey"  
-  ami  = "ami-0fd9f6305e5904533"
+  ami  = "ami-0c5862adf6801c45e"
   subnet_id = "subnet-015bab2e22a25f13d"
   tags = {
     Name  = "uat-server-01"
